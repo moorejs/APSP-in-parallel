12e381c6673265ffb1f78584725ff52778df19ba
Jared Moore <jmoore1@andrew.cmu.edu>
2018-04-15 16:59:50 -0400

    Added correctness check switch

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "floyd_warshall.h"
 
 void print_usage();
-std::string get_solution_filename(int n, double p, unsigned long seed);
+std::string get_solution_filename(std::string prefix, int n, double p, unsigned long seed);
 
 int main(int argc, char* argv[]) {
   unsigned long seed;
@@ -18,10 +18,11 @@ int main(int argc, char* argv[]) {
   int n = 1000;
   double p = 0.5;
   bool use_floyd_warshall = true;
+  bool check_correctness = false;
 
   extern char* optarg;
   int opt;
-  while ((opt = getopt(argc, argv, "han::p:s:")) != -1) {
+  while ((opt = getopt(argc, argv, "han::p:s:c")) != -1) {
     switch (opt) {
       case 'h':
         print_usage();
@@ -47,49 +48,56 @@ int main(int argc, char* argv[]) {
       case 's':
         seed = std::stoul(optarg);
         break;
+
+      case 'c':
+        check_correctness = true;
+        break;
     }
   }
 
   if (use_floyd_warshall) {
     std::cout << "\nGenerating " << n << "x" << n << " adjacency matrix with seed " << seed << "\n";
 
-    bool write_solution_to_file = true;
+    int* solution;
+    if (check_correctness) {
+      bool write_solution_to_file = true;
 
-    // have we cached the solution before?
-    std::string solution_filename = get_solution_filename(n, p, seed);
-    bool solution_available = stat(solution_filename.c_str(), nullptr) != -1 && errno != ENOENT;
+      // have we cached the solution before?
+      std::string solution_filename = get_solution_filename("fw", n, p, seed);
+      bool solution_available = stat(solution_filename.c_str(), nullptr) != -1 && errno != ENOENT;
 
-    int* solution = new int[n * n];
-    if (solution_available) {
-      std::cout << "Reading reference solution from file\n";
+      solution = new int[n * n];
+      if (solution_available) {
+        std::cout << "Reading reference solution from file\n";
 
-      std::ifstream in(solution_filename, std::ios::in | std::ios::binary);
-      in.read(reinterpret_cast<char*>(solution), n * n * sizeof(int));
-      in.close();
-    } else {
-      std::cout << "Solving APSP with Floyd-Warshall sequentially for reference solution\n";
+        std::ifstream in(solution_filename, std::ios::in | std::ios::binary);
+        in.read(reinterpret_cast<char*>(solution), n * n * sizeof(int));
+        in.close();
+      } else {
+        std::cout << "Solving APSP with Floyd-Warshall sequentially for reference solution\n";
 
-      int* matrix = floyd_warshall_init(n, p, seed);
+        int* matrix = floyd_warshall_init(n, p, seed);
 
-      auto start = std::chrono::high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
 
-      floyd_warshall(matrix, solution, n);
+        floyd_warshall(matrix, solution, n);
 
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double, std::milli> start_to_end = end - start;
-      std::cout << "Algorithm runtime: " << start_to_end.count() << "ms\n";
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> start_to_end = end - start;
+        std::cout << "Algorithm runtime: " << start_to_end.count() << "ms\n";
 
-      if (write_solution_to_file) {
-        std::cout << "Writing solution to file\n";
+        if (write_solution_to_file) {
+          std::cout << "Writing solution to file\n";
 
-        if (system("mkdir -p solution_cache") == -1) {
-          std::cerr << "mkdir failed!";
-          return -1;
+          if (system("mkdir -p solution_cache") == -1) {
+            std::cerr << "mkdir failed!";
+            return -1;
+          }
+
+          std::ofstream out(solution_filename, std::ios::out | std::ios::binary);
+          out.write(reinterpret_cast<const char*>(solution), n * n * sizeof(int));
+          out.close();
         }
-
-        std::ofstream out(solution_filename, std::ios::out | std::ios::binary);
-        out.write(reinterpret_cast<const char*>(solution), n * n * sizeof(int));
-        out.close();
       }
     }
 
@@ -106,19 +114,22 @@ int main(int argc, char* argv[]) {
     std::chrono::duration<double, std::milli> start_to_end = end - start;
     std::cout << "Algorithm runtime: " << start_to_end.count() << "ms\n\n";
 
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (output[i*n + j] != solution[i*n + j]) {
-          std::cerr << "Output did not match at [" << i << "][" << j << "]: " << output[i*n+j] 
-              << " vs solution's " << solution[i*n+j] << "!\n";
+    if (check_correctness) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (output[i*n + j] != solution[i*n + j]) {
+            std::cerr << "Output did not match at [" << i << "][" << j << "]: " << output[i*n+j] 
+                << " vs solution's " << solution[i*n+j] << "!\n";
+          }
         }
       }
     }
 
     delete[] matrix;
-    delete[] solution;
     delete[] output;
+    if (check_correctness) {
+      delete[] solution;
+    }
   } else {
   }
 
@@ -126,7 +137,7 @@ int main(int argc, char* argv[]) {
 }
 
 void print_usage() {
-  std::cout << "\nUsage: asap [-n INT] [-p DOUBLE] [-a (f|j)] [-s ULONG]\n";
+  std::cout << "\nUsage: asap [-n INT] [-p DOUBLE] [-a (f|j)] [-s ULONG] [-c]\n";
   std::cout << "\t-h\t\tPrint this message\n";
   std::cout << "\t-n INT\t\tGraph size, default 1000\n";
   std::cout << "\t-p DOUBLE\t\tProbability of edge from a given node to another (0.0 to 1.0), default 0.5\n";
@@ -134,11 +145,12 @@ void print_usage() {
   std::cout << "\t\t\t\tf: Floyd-Warshall (default)\n";
   std::cout << "\t\t\t\tj: Johnson's Algorithm\n";
   std::cout << "\t-s ULONG\t\tSeed for graph generation\n";
+  std::cout << "\t-c\t\tCheck correctness\n";
   std::cout << "\n";
 }
 
-std::string get_solution_filename(int n, double p, unsigned long seed) {
+std::string get_solution_filename(std::string prefix, int n, double p, unsigned long seed) {
   std::stringstream solution_filename;
-  solution_filename << "solution_cache/fw-sol-n"<< n << "-p" << p << "-s" << seed << ".bin";
+  solution_filename << "solution_cache/" << prefix << "-sol-n"<< n << "-p" << p << "-s" << seed << ".bin";
   return solution_filename.str();
 }
