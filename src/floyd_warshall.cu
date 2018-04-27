1f3a8e44d46fa172cc0f0135d898371a85a2ccb4
Josh Kalapos <jkalapos@andrew.cmu.edu>
2018-04-27 13:30:45 -0400

    Starting FW cuda code

--- src/floyd_warshall.cu
+++ src/floyd_warshall.cu
@@ -1,4 +1,5 @@
 #include <iostream> // cout
+#include <algorithm>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
@@ -18,7 +19,7 @@ __host__ void floyd_warshall_blocked_cuda() {
   // from assignment 1
   int deviceCount;
   cudaGetDeviceCount(&deviceCount);
-  
+
   for (int i = 0; i < deviceCount; i++) {
     cudaDeviceProp deviceProps;
     cudaGetDeviceProperties(&deviceProps, i);
@@ -28,4 +29,62 @@ __host__ void floyd_warshall_blocked_cuda() {
 	      << "\tGlobal mem: " << static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024 * 1024) << "GB \n"
 	      << "\tCUDA Cap: " << deviceProps.major << "." << deviceProps.minor << "\n";
   }
+
+}
+
+__global__ void floyd_warshall_kernel(int n, int* graph) {
+
+  int in = (threadIdx.x + blockIdx.x * blockDim.x) * n;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+  int inj = in + j;
+
+  for (int k = 0; k < n; k++) {
+    if (graph[inj] > graph[in + k] + graph[k*n + j]) {
+      graph[inj] = graph[in + k] + graph[k*n + j];
+    }
+    __syncthreads();
+  }
+
+}
+
+__host__ void floyd_warshall_cuda(int* input, int* output, int n) {
+  // from assignment 1
+  int deviceCount;
+  cudaGetDeviceCount(&deviceCount);
+
+  for (int i = 0; i < deviceCount; i++) {
+    cudaDeviceProp deviceProps;
+    cudaGetDeviceProperties(&deviceProps, i);
+
+    std::cout << "Device " << i << ": " << deviceProps.name << "\n"
+	      << "\tSMs: " << deviceProps.multiProcessorCount << "\n"
+	      << "\tGlobal mem: " << static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024 * 1024) << "GB \n"
+	      << "\tCUDA Cap: " << deviceProps.major << "." << deviceProps.minor << "\n";
+  }
+
+  int* device_graph;
+
+  const size_t size = sizeof(int) * n;
+
+  cudaMalloc(&device_graph, size);
+
+  cudaMemcpy(input, device_graph, size, cudaMemcpyHostToDevice);
+
+  dim3 block_dim(32, 32, 1);
+  dim3 grid_dim((n + block_dim.x - 1) / block_dim.x,
+                (n + block_dim.y - 1) / block_dim.y);
+
+  floyd_warshall_kernel<<<grid_dim, block_dim>>>(n, device_graph);
+  cudaThreadSynchronize();
+
+  cudaMemcpy(device_graph, output, size, cudaMemcpyDeviceToHost);
+
+  cudaError_t errCode = cudaPeekAtLastError();
+  if (errCode != cudaSuccess) {
+      std::cerr << "WARNING: A CUDA error occured: code=" << errCode << "," <<
+                cudaGetErrorString(errCode) << "\n";
+  }
+
+  cudaFree(device_graph);
+
 }
